797bee8c670d650814a4def440caffef9f7ad30c
Paulus Esterhazy <pesterhazy@gmail.com>
2026-02-03 11:13:51 +0100

    WIP

--- src/aoc.test.ts
+++ src/aoc.test.ts
@@ -16,5 +16,5 @@ function parseInput09(input: string): { x: number; y: number }[] {
 
 test("parseInput09", () => {
   const input = parseInput09(example09);
-  expect(input.length).toBe(8);
+  assert.equal(input.length, 2);
 });
