f89c2f22de3f8b3bded1f3603bfc603928fd9b3d
Paulus Esterhazy <pesterhazy@gmail.com>
2026-02-03 10:50:47 +0100

    WIP

---
+++ scripts/watch.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env bun
+
+import { watch } from "fs";
+
+const watcher = watch(".", { recursive: true }, (event, filename) => {
+  console.log(`Detected ${event} in ${filename}`);
+});
+
+process.on("SIGINT", () => {
+  console.log("\nClosing watcher...");
+  watcher.close();
+  process.exit(0);
+});
--- src/aoc.test.ts
+++ src/aoc.test.ts
@@ -1,3 +1,19 @@
+const example09 = `7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3`;
+
+function parseInput09(input: string): { x: number; y: number }[] {
+  return input.split("\n").map((line) => {
+    const [x, y] = line.split(",").map(Number);
+    return { x, y };
+  });
+}
 test("should work", () => {
-  assert.equal(1, 1);
+  const input = parseInput09(example09);
+  assert.equal(input.length, 8);
 });
